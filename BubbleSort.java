1dc792307e5dc38599cae9da038a38e2bbf83cb7
mabatemarco <mabatemarco928@gmail.com>
2020-10-28 14:21:38 -0400

    var names

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
 class BubbleSort {
-  
-  void bubbleSort(int arr[]) {
+
+  void sort(int arr[]) {
     int n = arr.length;
     for (int i = 0; i < n - 1; i++) {
       for (int j = 0; j < n - i - 1; j++) {
@@ -19,6 +19,6 @@ class BubbleSort {
   public static void main(String args[]) {
     BubbleSort ob = new BubbleSort();
     int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    ob.bubbleSort(arr);
+    ob.sort(arr);
   }
 }
--- InsertionSort.java
+++ InsertionSort.java
@@ -5,13 +5,13 @@ class InsertionSort {
   void sort(int arr[]) {
     int n = arr.length;
     for (int i = 1; i < n; i++) {
-      int key = arr[i];
+      int current = arr[i];
       int j = i - 1;
-      while (j >= 0 && arr[j] > key) {
+      while (j >= 0 && arr[j] > current) {
         arr[j + 1] = arr[j];
         j = j - 1;
       }
-      arr[j + 1] = key;
+      arr[j + 1] = current;
     }
     
     System.out.println(Arrays.toString(arr));
